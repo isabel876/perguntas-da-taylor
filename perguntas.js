4d3a44a9e82b729e817f0e0390ba3340387c1157
isabel876 <00001122234533sp@al.educacao.sp.gov.br>
2024-11-22 15:59:49 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,71 @@
+criaCartao(
+    'Geografia', 
+    'Qual é a capital da Italia?',
+    'Roma'
+)
+
+criaCartao(
+    'História', 
+    'Quem foi o primeiro presidente dos Estados Unidos?',
+    'George Washington'
+)
+
+criaCartao(
+    'Ciência', 
+    'Qual planeta é conhecido como o "Planeta Vermelho"?',
+    'Marte'
+)
+
+criaCartao(
+    'História',
+    'De quem é a famosa frase “Penso, logo existo”?',
+    'Descartes'
+)
+
+criaCartao(
+    'Matemática',
+    'Quantas casas decimais tem o número pi?',
+    'Infinitas'
+)
+
+criaCartao(
+    'História',
+    'Qual famoso festival acontece em outubro e envolve cerveja?',
+    'Oktoberfest'
+)
+
+criaCartao(
+    'Química', 
+    'Atualmente, quantos elementos químicos a tabela periódica possui?',
+    '118'
+)
+
+criaCartao(
+    'Ingles', 
+    'O que a palavra legend significa em português?',
+    'Lenda'
+)
+
+criaCartao(
+    'Ciência', 
+    'Quanto tempo a luz do Sol demora para chegar à Terra?',
+    '8 minutos'
+)
+
+criaCartao(
+    'Geografia', 
+    'Qual a montanha mais alta do Brasil?',
+    'Pico da Neblina'
+)
+
+criaCartao(
+    'Filosofia', 
+    'Qual foi o recurso utilizado inicialmente pelo homem para explicar a origem das coisas?',
+    'A Mitologia.'
+)
+
+criaCartao(
+    'Ciência', 
+    'Qual o maior animal terrestre?',
+    'Elefante africano.'
+)
